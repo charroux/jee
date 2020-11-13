1588df8f446bca5f19a30f2921e57170ba19eff6
orchaland <orchaland@gmail.com>
2020-11-13 12:54:20 +0100

    fist commit

---
+++ src/main/java/com/example/CarRental/CarRentalService.java
@@ -0,0 +1,53 @@
+package com.example.CarRental;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+
+@RestController
+public class CarRentalService {
+	
+	private List<Car> cars = new ArrayList<Car>();
+	
+	public CarRentalService() {
+		cars.add(new Car("11AA22", "Ferrari", 1000));
+		cars.add(new Car("33BB44", "Porshe", 2222));
+	}
+	
+	@GetMapping("/cars")
+	public List<Car> getListOfCars(){
+		return cars;
+	}
+	
+	@PostMapping("/cars")
+	public void addCar(@RequestBody Car car) throws Exception{
+		System.out.println(car);
+		cars.add(car);
+	}
+
+	@GetMapping("/cars/{plateNumber}")
+	public Car getCar(@PathVariable(value = "plateNumber") String plateNumber){
+		for(Car car: cars){
+			if(car.getPlateNumber().equals(plateNumber)){
+				return car;
+			}
+		}
+		return null;
+	}
+
+	@PutMapping("/cars/{plateNumber}")
+	public void rent(@PathVariable("plateNumber") String plateNumber, @RequestParam(value="rent",
+			required = true)boolean rent, @RequestBody Dates dates){
+		for(Car car: cars){
+			if(car.getPlateNumber().equals(plateNumber)){
+				car.setDates(dates);
+				car.setRented(true);
+			}
+		}
+	}
+
+}
