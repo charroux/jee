1d90bb905c8fe6927f45728b908882d9d296dad4
orchaland <orchaland@gmail.com>
2020-11-19 14:47:29 +0100

    repository

--- src/main/java/com/example/CarRental/CarRentalService.java
+++ src/main/java/com/example/CarRental/CarRentalService.java
@@ -4,34 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
 @RestController
 public class CarRentalService {
 	
-	private List<Car> cars = new ArrayList<Car>();
+	// private List<Car> cars = new ArrayList<Car>();
 	
-	public CarRentalService() {
-		cars.add(new Car("11AA22", "Ferrari", 1000));
-		cars.add(new Car("33BB44", "Porshe", 2222));
+	CarRepository carRepository;
+	
+	@Autowired
+	public CarRentalService(CarRepository carRepository) {
+		this.carRepository = carRepository;
+		Car car = new Car("11AA22", "Ferrari", 1000);
+		carRepository.save(car);
+		car = new Car("33BB44", "Porshe", 2222);
+		carRepository.save(car);
 	}
 	
 	@GetMapping("/cars")
 	public List<Car> getListOfCars(){
-		return cars;
+		return carRepository.findAll(); // <=> select * from Car
 	}
 	
 	@PostMapping("/cars")
 	public void addCar(@RequestBody Car car) throws Exception{
 		System.out.println(car);
-		cars.add(car);
+		carRepository.findAll().add(car);
 	}
 
 	@GetMapping("/cars/{plateNumber}")
 	public Car getCar(@PathVariable(value = "plateNumber") String plateNumber){
-		for(Car car: cars){
+		for(Car car: carRepository.findAll()){
 			if(car.getPlateNumber().equals(plateNumber)){
 				return car;
 			}
@@ -42,7 +48,7 @@ public class CarRentalService {
 	@PutMapping("/cars/{plateNumber}")
 	public void rent(@PathVariable("plateNumber") String plateNumber, @RequestParam(value="rent",
 			required = true)boolean rent, @RequestBody Dates dates){
-		for(Car car: cars){
+		for(Car car: carRepository.findAll()){
 			if(car.getPlateNumber().equals(plateNumber)){
 				car.setDates(dates);
 				car.setRented(true);
--- src/main/java/com/example/CarRental/CarRepository.java
+++ src/main/java/com/example/CarRental/CarRepository.java
@@ -1,6 +1,12 @@
 package com.example.CarRental;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository<Car, String> {
+	
+	@Override
+	List<Car> findAll();
+	
 }
